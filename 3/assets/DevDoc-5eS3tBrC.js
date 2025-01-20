0a306c2425af6623b22a0a4b6d495c6e7609dabf
IceKSener <1878024009@qq.com>
2025-01-20 23:39:13 +0800

    更新网站

---
+++ 3/assets/DevDoc-5eS3tBrC.js
@@ -0,0 +1,28 @@
+import{_ as s,r as o,h as c,M as i,H as t,m as l,o as _,c as d,b as g}from"./index-D1t46wQ3.js";const h=`## markdown文档\r
+\`\`\`js\r
+console.log("这是Markdown吗？")\r
+let OK="应该是"\r
+\`\`\`\r
+\r
+$10086^2$\r
+\r
+---\r
+\r
+# 文档\r
+## API\r
+### login\r
+\r
+#### 请求样例\r
+\`\`\`\r
+http://example.com/api/login?n=123&key=666\r
+\`\`\`\r
+\r
+| 方法 | POST |\r
+|---|---|\r
+\r
+| 参数 | 类型 | 描述 | 必选 |\r
+|---|---|---|---|\r
+| n | string | 用户名 | yes |\r
+| key | string | 密钥 | yes |\r
+\r
+666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666`,u={class:"area"},m=["innerHTML"],p={__name:"DevDoc",setup(v){const e=o("");return c(()=>{const n=new i({highlight:(a,r)=>{if(r&&t.getLanguage(r))try{return t.highlight(a,{language:r}).value}catch{}return""}});n.use(l),e.value=n.render(h)}),(n,a)=>(_(),d("div",u,[g("div",{class:"md",innerHTML:e.value},null,8,m)]))}},M=s(p,[["__scopeId","data-v-337295b0"]]);export{M as default};
