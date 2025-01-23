7d5bba574c49ade8caf5fc67dcbe846e871cb48c
IceKSener <1878024009@qq.com>
2025-01-23 09:15:45 +0800

    更新网站

---
+++ 6/assets/DevDoc-BLXzaMuz.js
@@ -0,0 +1,32 @@
+import{_ as r,a as e,o as s,c as a,i as o,d as t}from"./index-CPqhdPw1.js";const c=`# 文档\r
+## API\r
+### 登录\r
+\r
+#### 请求样例\r
+\`\`\`javascript\r
+fetch('http://example.com/api/token",{\r
+    method:'POST',\r
+    headers:{\r
+        "Content-type": "application/json"\r
+    }\r
+    body:JSON.stringify({\r
+        username:"name",\r
+        password:"password"\r
+    })\r
+})\r
+\`\`\`\r
+\r
+| 方法 | POST |\r
+|---|---|\r
+\r
+| 参数 | 类型 | 描述 | 必选 |\r
+|---|---|---|---|\r
+| username | string | 用户名 | yes |\r
+| password | string | 密码 | yes |\r
+\r
+#### 返回\r
+\r
+| 参数 | 类型 | 描述 |\r
+|---|---|---|\r
+| a | string | a |\r
+| r | string | r |`,p={class:"area"},d={__name:"DevDoc",setup(_){return(m,i)=>{const n=e("Markdown");return s(),a("div",p,[o(n,{class:"md",md:t(c)},null,8,["md"])])}}},u=r(d,[["__scopeId","data-v-b8470936"]]);export{u as default};
