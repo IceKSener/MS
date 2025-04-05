bc727b8d26e3e2f613fd1322c6a42286dad6d353
IceKSener <1878024009@qq.com>
2025-04-05 16:53:16 +0800

    最新编译版

---
+++ assets/KgFinder-BWaaBl22.js
@@ -0,0 +1,1 @@
+import{_ as r,c as a,o as t,a as s}from"./index-BeX7OZKs.js";const c={data(){return{iframeSrc:"./kgshow/GraphFinder.html"}}},o={class:"iframe-container"},n=["src"];function i(_,d,f,m,e,p){return t(),a("div",o,[s("iframe",{src:e.iframeSrc,frameborder:"0",width:"100%",height:"100%"},null,8,n)])}const l=r(c,[["render",i],["__scopeId","data-v-60ae5760"]]);export{l as default};
