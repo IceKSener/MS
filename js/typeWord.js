2dcb8d96e03baf148845e45ff4e3541db192b581
IceKSener <1878024009@qq.com>
2025-04-07 10:51:36 +0800

    新编译

--- js/typeWord.js
+++
@@ -1,8 +0,0 @@
-text=[]
-onmessage=(e)=>{
-    e=e.data
-    if(e[0]===0) text.push(e[1])
-    else if(e[0]===1){ let x=text; text=[]; postMessage(x.join('')) }
-    else if(e[0]===2) text=[]
-    return
-}
