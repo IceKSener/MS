de86bff06eaef840e9bd481e617f278ec7c32435
IceKSener <1878024009@qq.com>
2025-04-08 16:59:54 +0800

    网站更新

---
+++ 29/assets/KgNavigator-zFNdpwc9.js
@@ -0,0 +1,1 @@
+import{_ as r,c as a,o as t,i as o}from"./index-DLkJZCO8.js";const c={data(){return{iframeSrc:"./kgshow/GraphNavigator.html"}}},s={class:"iframe-container"},i=["src"];function n(_,f,d,m,e,p){return t(),a("div",s,[o("iframe",{src:e.iframeSrc,frameborder:"0",width:"100%",height:"100%"},null,8,i)])}const l=r(c,[["render",n],["__scopeId","data-v-68b8ff6c"]]);export{l as default};
